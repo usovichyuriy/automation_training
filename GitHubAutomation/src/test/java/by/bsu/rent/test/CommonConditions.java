9d94c93c6bf03d0fe1308afbe2a41ffd7a8e732b
Yuriy Usovich <36422364+usovichyuriy@users.noreply.github.com>
2019-12-24 08:19:55 +0300

    some fixes

--- GitHubAutomation/src/test/java/by/bsu/rent/test/CommonConditions.java
+++ GitHubAutomation/src/test/java/by/bsu/rent/test/CommonConditions.java
@@ -1,8 +1,8 @@
 package by.bsu.rent.test;
 
 
-import by.bsu.rent.driver.DriverSingleton;
 import by.bsu.rent.util.TestListener;
+import by.bsu.rent.driver.DriverSingleton;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,17 +10,16 @@ import org.testng.annotations.Listeners;
 
 @Listeners({TestListener.class})
 public class CommonConditions {
+
     protected WebDriver driver;
-    private static final String RESOURCES_PATH = "src\\test\\resources\\";
 
-    @BeforeMethod
-    public void setUpDriver() {
+    @BeforeMethod()
+    public void setUp() {
         driver = DriverSingleton.getDriver();
-
     }
-    @AfterMethod (alwaysRun = true)
-    public void tearDownMozillaDriver() {
+
+    @AfterMethod(alwaysRun = true)
+    public void stopBrowser() {
         DriverSingleton.closeDriver();
     }
-
 }
